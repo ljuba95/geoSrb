e008db7efd37e332c3955d0afb0cd8704563e1de
pkaruovic <pkaruovic@gmail.com>
2017-12-02 12:00:40 +0100

    Sad ga ima

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,12 @@
+<header class="masthead" style="background-image: url('img/srb.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>GEO SRBIJA </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,13 @@
+<nav id="sidebar-nav">
+  <ul>
+      <a href="#" class="list-group-item">Pocetna</a>
+      <a href="#" class="list-group-item">Kartografski pregled</a>
+      <a href="#" class="list-group-item">Pregled metapodataka</a>
+      <a href="#" class="list-group-item">Osnovno o NIGP-u</a>
+      <a href="#" class="list-group-item">INSPIRE</a>
+      <a href="#" class="list-group-item">Novosti</a>
+      <a href="#" class="list-group-item">Partneri</a>
+      <a href="#" class="list-group-item">Termini</a>
+      <a href="#" class="list-group-item">Kontakt</a>
+  </ul>
+</nav>
