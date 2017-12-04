f8422fbf5861150f071ec6a1390318521a0e3ae6
pkaruovic <pkaruovic@gmail.com>
2017-12-04 01:50:16 +0100

    File upload in progress

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\File;
+use App\FileCategory;
+
+class FilesController extends Controller
+{
+    function index($id){
+        $category = FileCategory::find($id);
+        return json_encode($category->files);
+    }
+
+
+}
