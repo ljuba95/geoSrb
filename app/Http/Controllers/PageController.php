fb29566d4db36c68a3351b2e77e387218ae0f717
Milos Ljubisavljevic <ljuba95@hotmail.com>
2017-12-21 18:01:05 +0100

    povezao strane i meni, bez jezika

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\LinkGroup;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use TCG\Voyager\Models\Page;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $pages = Page::all();
+        $json = array();
+
+        foreach($pages as $page){
+            array_push($json, array('id' => $page->id, 'title' => $page->title));
+        }
+        return json_encode($json);
+    }
+
+    public function show($id){
+        $pageContent = DB::table('pages')->where('id', $id)->first();
+        $jezik = LaravelLocalization::getCurrentLocale();
+        $linkGroups = LinkGroup::all();
+        $grupeSaJezikom = array();
+        foreach ($linkGroups as $group) {
+            if($group->language->name == $jezik && $group->active){
+                $grupeSaJezikom[] = $group;
+            }
+        }
+        return view('home.pages', ['pageContent' => $pageContent, 'linkGroups' => $grupeSaJezikom]);
+    }
+
+    public static function getMenuItems(){
+        $pages = Page::all();
+
+        foreach ($pages as $page) {
+            $menu_items[] = array('url' => '/pages/' . $page->id, 'title' => $page->title);
+        }
+
+        return $menu_items;
+    }
 }
